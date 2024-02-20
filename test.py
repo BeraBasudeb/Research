fa608bc1cd4451c8f64e30d41cc7930acb2bd920
Bera Basudeb <160571815+BeraBasudeb@users.noreply.github.com>
2024-02-20 13:55:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = ['Mishra et al.', 'Rewal et al.', 'Islam and Basu','Dabra et al.','Feng et al.', 'Zhang et al.', 'Chang et al.', 'Irshad et al.', 'QRAC-IoD']
+X = ['x', 'y', 'z','D','F', 'Z', 'C', 'I', 'Q']
 Ymsg = [3, 4, 6, 3, 3, 4, 4, 3, 3]
 Zcomm = [14.530, 20.162, 20.546, 9.890, 9.730, 14.336, 10.752, 15.008, 11.521]
 
